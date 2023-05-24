5407d1d2aa252b8a2e94ed832ba539a582f1694f
Ashenafi <ashenafigodanaj@gmail.com>
2023-05-24 03:51:14 -0700

    modified version of the app for code readablity and efficiency

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 weight = float(input('Weight: '))
 unit = input('(L)bs or (K)g: ')
 
-if unit == 'l' or unit == 'L':
+if unit.upper() == 'L':
     result = 0.45 * weight
     print(f'You are {result} kilos')
-elif unit == 'k' or unit == 'K':
+elif unit.upper() == 'K':
     result = weight / 0.45
     print(f'You are {result} pounds')
